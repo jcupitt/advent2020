85ae7ee9f9d69050a21b30f8ee60b0d5ef5a8a0c
John Cupitt <jcupitt@gmail.com>
2020-12-02 14:00:39 +0000

    use eor

--- day2/part2.rb
+++ day2/part2.rb
@@ -3,15 +3,12 @@
 n_correct = 0
 STDIN.each_line do |line|
   line !~ /(\d+)-(\d+) (.): ([a-z]+)/
-  f994454-bffc0b14irst = $~[1].to_i - 1
+  first = $~[1].to_i - 1
   second = $~[2].to_i - 1
   letter = $~[3]
   password = $~[4].chars
 
-  if (password[first] == letter && password[second] != letter) ||
-    (password[first] != letter && password[second] == letter)
-    n_correct += 1
-  end
+  n_correct += 1 if (password[first] == letter) ^ (password[second] == letter)
 end
 
 puts n_correct
