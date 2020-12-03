77c9d4ae0cf076d6bcfd0d6c23cbd570f7e2b2fa
John Cupitt <jcupitt@gmail.com>
2020-12-03 12:18:44 +0000

    removed loops in part2

--- day3/part2.rb
+++ day3/part2.rb
@@ -3,15 +3,11 @@
 input = STDIN.each_line.map(&:chomp)
 
 result = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].map do |dx, dy|
-  x = y = n_trees = 0
-
-  while y < input.length
-    n_trees += 1 if input[y][x % input[0].length] == "#"
-    x += dx
-    y += dy
-  end
-
-  n_trees
+  (0 ... input.length).step(dy).zip((0 ...).step(dx)).map do |y, x|
+    input[y][x % input[0].length]
+  end.
+    filter{|ch| ch == "#"}.
+    count
 end.reduce(:*)
 
 puts result
