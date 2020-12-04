a2f5953ca67b9b21715e8c4bc24346fb547d0cb0
John Cupitt <jcupitt@gmail.com>
2020-12-04 14:22:45 +0000

    start part2

--- day4/part2.rb
+++ day4/part2.rb
@@ -1,8 +1,66 @@
 #!/usr/bin/ruby
 
+$validation_patterns = {
+  "byr" => /(\d{4})/,
+  "iyr" => /(\d{4})/,
+  "eyr" => /(\d{4})/,
+  "hgt" => /(\d+)(cm|in)/,
+  "hcl" => /#[0-9a-f]{6}/,
+  "ecl" => /(amb|blu|brn|gry|grn|hzl|oth)/,
+  "pid" => /(\d{9})/
+}
+
+def validate_range(lower, upper)
+  def check(v1, v2)
+    puts "in range check"
+    v1.to_i >= lower && v1.to_i <= upper
+  end
+end
+
+def validate_height
+  def check(v1, v2)
+    puts "in height check"
+    if v2 == "in"
+      fn = validate_range(59, 76)
+      fn(v1, v2)
+    else
+      fn = validate_range(150, 193)
+      fn(v1, v2)
+    end
+  end
+end
+
+$validation_fns = {
+  "byr" => validate_range(1920, 2002),
+  "iyr" => validate_range(2010, 2020),
+  "eyr" => validate_range(2020, 2030),
+  "hgt" => validate_height
+}
+
 def valid(passport)
-  passport.length == 8 || 
-    (passport.length == 7 && !passport.include?("cid"))
+  valid = true
+  puts "passport = #{passport}"
+  $validation_patterns.each do |key, pattern|
+    if ! passport.key? key
+      puts "passport does not include #{key}"
+      valid = false
+    else
+      if passport[key] !~ pattern
+        puts "passport value #{passport[key]} does not match #{pattern}"
+        valid = false
+      else
+        if $validation_fns.include?(key)
+          fn = $validation_fns[key]
+          puts "checking #{key}.."
+          if !fn($~[1], $~[2])
+            valid = false
+          end
+        end
+      end
+    end
+  end
+
+  valid
 end
 
 n_valid = 0
