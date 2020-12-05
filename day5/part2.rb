94c812a393538bb0baf30effcd6a641dc50f17f8
John Cupitt <jcupitt@gmail.com>
2020-12-05 06:37:09 +0000

    nicer set usage

--- day5/part2.rb
+++ day5/part2.rb
@@ -2,11 +2,9 @@
 
 require "set"
 
-seat_map = Set.new
-
-STDIN.map do |line|
-  seat_map.add line[..9].chars.
+seat_map = STDIN.map do |line|
+  line[..9].chars.
     map{|x| (x == "F" || x == "L") ? 0 : 1}.reduce{|s, x| s << 1 | x}
 end
 
-puts Set.new(seat_map.min .. seat_map.max) - seat_map
+puts (seat_map.min .. seat_map.max).to_set - seat_map.to_set
