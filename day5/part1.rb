dd6ef50ccd592a56a8476006bc56d03609df7ab8
John Cupitt <jcupitt@gmail.com>
2020-12-05 06:12:24 +0000

    tiny cleanups

--- day5/part1.rb
+++ day5/part1.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/ruby
 
-highest = STDIN.map do |line|
+puts STDIN.map do |line|
   line[..9].chars.
     map{|x| (x == "F" || x == "L") ? 0 : 1}.reduce{|s, x| s << 1 | x}
 end.max
-
-puts highest
-
--- day5/part2.rb
+++ day5/part2.rb
@@ -3,17 +3,12 @@
 seat_map = Array.new(832){0}
 
 STDIN.map do |line|
-  id = line[..9].chars.
-    map{|x| (x == "F" || x == "L") ? 0 : 1}.reduce{|s, x| s << 1 | x}
-  seat_map[id] = 1
+  seat_map[line[..9].chars.
+    map{|x| (x == "F" || x == "L") ? 0 : 1}.reduce{|s, x| s << 1 | x}] = 1
 end
 
 (1 ... seat_map.length).each do |id|
-  if seat_map[id - 1] == 1 && 
-    seat_map[id] == 0 && 
-    seat_map[id + 1] == 1
-    puts id
-  end
+  puts id if seat_map[id - 1] == 1 && seat_map[id] == 0 && seat_map[id + 1] == 1
 end
 
 
