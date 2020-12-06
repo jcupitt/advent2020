de9dc9e23804474770f55710a992fbc3e12bc4b6
John Cupitt <jcupitt@gmail.com>
2020-12-06 12:38:00 +0000

    all done

---
+++ day6/part2.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+def read_groups(input)
+  group = []
+  input.each_line do |line|
+    line = line.chomp
+    if line == ""
+      yield group if group != []
+      group = []
+      next
+    end
+    group << line.chars.map(&:to_sym)
+  end
+  yield group if group != []
+end
+
+group_counts = []
+read_groups(STDIN) do |group|
+  counts = Hash.new 0
+  group.each{|person| person.each{|question| counts[question] += 1}}
+  group_counts << 
+    counts.values.map{|value| value == group.length ? 1 : 0}.reduce(:+)
+end
+
+puts group_counts.reduce :+
